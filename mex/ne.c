7540d1916f61c229c61eb8fd7237f2927ec38d6b
Peter Corke <peter.i.corke@gmail.com>
2012-02-22 10:29:32 +0000

    Fix bug in prismatic case. Fix friction sign to be consistent with m-file version.

--- mex/ne.c
+++ mex/ne.c
@@ -287,9 +287,10 @@ newton_euler (
 			/*
 			 * compute acc[j]
 			 */
-			if (j == 0)
+			if (j == 0) {
+				vect_add(&qddv, &qddv, robot->gravity);
 				rot_trans_vect_mult(ACC(j), ROT(j), &qddv);
-			else {
+			} else {
 				vect_add(&t1, &qddv, ACC(j-1));
 				rot_trans_vect_mult(ACC(j), ROT(j), &t1);
 
@@ -462,7 +463,7 @@ newton_euler (
 		 * add actuator dynamics and friction
 		 */
 		t +=   l->G * l->G * l->Jm * qdd[j*stride]; // inertia
-        t -= fabs(l->G) * (
+        t += fabs(l->G) * (
             l->B * qd[j*stride] +    // viscous friction
 			(qd[j*stride] > 0 ? l->Tc[0] : 0.0) +    // Coulomb friction
 			(qd[j*stride] < 0 ? l->Tc[1] : 0.0)
