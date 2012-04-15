2e0f8d985edfc3e93106b425f73611ff0bc0486b
Peter Corke <peter.i.corke@gmail.com>
2012-04-15 07:58:43 +0000

    Major doco changes. Removed hardwired vehicle model, now passed a Vehicle object.

--- RRT.m
+++ RRT.m
@@ -1,30 +1,34 @@
 %RRT Class for rapidly-exploring random tree navigation
 %
-% A concrete class that implements the RRT navigation algorithm.
-% This class subclasses the Navigation class.
+% A concrete subclass of the Navigation class that implements the rapidly
+% exploring random tree (RRT) algorithm.  This is a kinodynamic planner
+% that takes into account the motion constraints of the vehicle.
 %
-% Usage for subclass:
+% Methods::
 %
-%   rrt = RRT(occgrid, options)  create an instance object
+% plan         Compute the tree
+% path         Compute a path 
+% plot         Display the tree
+% display      Display the parameters in human readable form
+% char         Convert to string
 %
-%   rrt                     show summary statistics about the object
-%   rrt.visualize()         display the occupancy grid
+% Example::
 %
-%   rrt.plan(goal)          plan a path to coordinate goal
-%   rrt.path(start)         display a path from start to goal
-%   p = rrt.path(start)     return a path from start to goal
+%    rrt = RRT();           % create navigation object
+%    rrt.plan()             % create navigation tree
+%    rrt.path(start, goal)  % animate path from this start location
 %
-% Options::
-% 'npoints',N    Number of nodes in the tree
-% 'time',T       Period to simulate dynamic model toward random point
-% 'xrange',X     Workspace span in x-direction [xmin xmax]
-% 'yrange',Y     Workspace span in y-direction [ymin ymax]
-% 'goal',P       Goal position (1x2) or pose (1x3) in workspace
+% References::
+% - Randomized kinodynamic planning,
+%   S. LaValle and J. Kuffner, 
+%   International Journal of Robotics Research vol. 20, pp. 378-400, May 2001.
+% - Probabilistic roadmaps for path planning in high dimensional configuration spaces,
+%   L. Kavraki, P. Svestka, J. Latombe, and M. Overmars, 
+%   IEEE Transactions on Robotics and Automation, vol. 12, pp. 566-580, Aug 1996.
+% - Robotics, Vision & Control, Section 5.2.5,
+%   P. Corke, Springer 2011.
 %
-% Notes::
-% - The bicycle model is hardwired into the class (should be a parameter)
-% - Default workspace is between -5 and +5 in the x- and y-directions
-%
+% See also Navigation, PRM, DXform, Dstar, PGraph.
 
 % Peter Corke 8/2009.
 
@@ -39,12 +43,6 @@ classdef RRT < Navigation
         npoints         % number of points to find
         graph           % graph Object representing random nodes
 
-        vgoal
-        vstart
-        localGoal
-        localPath
-        gpath            % path through the graph
-
         sim_time        % path simulation time
         kine_model      % simulink model for kinematics
 
@@ -52,92 +50,106 @@ classdef RRT < Navigation
         yrange
         
         plant
+
+        speed
+        steermax
+        vehicle
     end
 
     methods
 
-        % constructor
-        function rrt = RRT(varargin)
+        function rrt = RRT(map, vehicle, varargin)
+        %RRT.RRT Create a RRT navigation object
+        %
+        % R = RRT.RRT(MAP, VEH, OPTIONS) is a rapidly exploring tree navigation
+        % object for a region with obstacles defined by the map object MAP.
+        %
+        % Options::
+        % 'npoints',N    Number of nodes in the tree
+        % 'time',T       Period to simulate dynamic model toward random point
+        % 'range',R      Specify rectangular bounds
+        %                - R scalar; X: -R to +R, Y: -R to +R
+        %                - R (1x2); X: -R(1) to +R(1), Y: -R(2) to +R(2)
+        %                - R (1x4); X: R(1) to R(2), Y: R(3) to R(4)
+        % 'goal',P       Goal position (1x2) or pose (1x3) in workspace
+        % 'speed',S      Speed of vehicle [m/s] (default 1)
+        % 'steermax',S   Maximum steer angle of vehicle [rad] (default 1.2)
+        %
+        % Notes::
+        % - Does not (yet) support obstacles, ie. MAP is ignored but must be given.
+        % - 'steermax' selects the range of steering angles that the vehicle will
+        %   be asked to track.  If not given the steering angle range of the vehicle
+        %   will be used.
+        % - There is no check that the steering range or speed is within the limits
+        %   of the vehicle object.
 
             % invoke the superclass constructor
             rrt = rrt@Navigation(varargin{:});
 
             rrt.graph = PGraph(3, 'distance', 'SE2');  % graph of points in SE(2)
+            rrt.vehicle = vehicle;
 
-            % TODO: need a means to set these
-            %  {'npoints', []
-            
             opt.npoints = 500;
             opt.time = 0.5;
-            opt.yrange = [-5,5];
-            opt.xrange = [-5,5];
+            opt.range = 5;
             opt.goal = [0, 0];
+            opt.steermax = [];
+            opt.speed = 1;
             
             [opt,args] = tb_optparse(opt, varargin);
             rrt.npoints = opt.npoints;
             rrt.sim_time = opt.time;
-            rrt.xrange = opt.xrange;
-            rrt.yrange = opt.yrange;
+
+            switch length(opt.range)
+            case 1
+                rrt.xrange = [-opt.range opt.range];
+                rrt.yrange = [-opt.range opt.range];
+            case 2
+                rrt.xrange = [-opt.range(1) opt.range(1)];
+                rrt.yrange = [-opt.range(2) opt.range(2)];
+            case 4
+                rrt.xrange = [opt.range(1) opt.range(2)];
+                rrt.yrange = [opt.range(3) opt.range(4)];
+            otherwise
+                error('bad range specified');
+            end
+            if ~isempty(opt.steermax)
+                rrt.steermax = opt.steermax;
+            else
+                rrt.steermax = vehicle.alphalim;
+            end
+            rrt.speed = opt.speed;
             rrt.goal = opt.goal;
         end
 
-        function plan(rrt)
+        function plan(rrt, varargin)
+        %RRT.plan Create a rapidly exploring tree
+        %
+        % R.plan(OPTIONS) creates the tree roadmap by driving the vehicle
+        % model toward random goal points.  The resulting graph is kept
+        % within the object.
+        %
+        % Options::
+        % 'goal',P        Goal pose (1x3)
+        % 'noprogress'    Don't show the progress bar
+        % 'samples'       Show samples
+        %                 - '.' for each random point x_rand
+        %                 - 'o' for the nearest point which is added to the tree
+        %                 - red line for the best path
 
+            opt.progress = true;
+            opt.samples = false;
+            opt.goal = [];
+            
+            opt = tb_optparse(opt, varargin);
+
+            if ~isempty(opt.goal)
+                rrt.goal = opt.goal;
+            end
 
             % build a graph over the free space
             rrt.message('create the graph');
             rrt.graph.clear();
-            rrt.create_graph();
-            disp('graph create done');
-
-            return
-            disp('plan still going');
-            rrt.vgoal = rrt.graph.closest(rrt.goal);
-
-            % find a path through the graph
-            rrt.message('planning path through graph');
-            rrt.graph.goal(rrt.vgoal);   % set the goal
-
-        end
-        
-
-    function p = path(rrt, xstart, xgoal)
-
-        g = rrt.graph;
-        vstart = g.closest(xstart)
-
-        vgoal = g.closest(xgoal)
-        g.goal(vstart);
-        p = g.path(vgoal)
-
-        pp = g.coord(p);
-
-        figure
-        clf
-        plot2(pp', '-o');
-        grid
-        xyzlabel
-
-        path = [];
-        for k= p(end-1:-1:1)
-            b = g.userdata{k};
-            path = [path b.path(:,1:b.k)];
-        end
-
-        clf
-        plot2(path');
-            xlabel('x');
-            ylabel('y');
-            zlabel('\theta');
-            grid
-        p = path;
-
-    end
-    
-    function next(rrt)
-    end
-    
-        function create_graph(rrt)
 
             if rrt.verbose
                 clf
@@ -145,24 +157,45 @@ classdef RRT < Navigation
                 hold on
             end
 
+            % check goal sanity
+            if isempty(rrt.goal)
+                error('no goal specified');
+            end
+            switch length(rrt.goal)
+            case 2
+                rrt.goal = [rrt.goal(:); 0];
+            case 3
+            otherwise
+                error('goal must be 3-vector');
+            end
+
             % add the goal point as the first node
-            rrt.graph.add_node([rrt.goal(:); 0]);
+            rrt.graph.add_node(rrt.goal);
 
-            j = 0;
-            h = waitbar(0, 'RRT planning...');
-            
-            clf; hold on
-            while j < rrt.npoints
+            % graphics setup
+            if opt.progress
+                h = waitbar(0, 'RRT planning...');
+            end
+            if opt.samples
+                clf
+                hold on
+                xlabel('x'); ylabel('y');
+            end
 
-                waitbar(j / rrt.npoints);
+            for j=1:rrt.npoints       % build the tree
+
+                if opt.progress
+                    waitbar(j / rrt.npoints);
+                end
                 
                 % Step 3
                 % find random state x,y
 
                 % pick a point not in obstacle
                 while true
-                    xy = rrt.randxy();
-                    ixy = round(xy);
+                    xy = rrt.randxy();  % get random coordinate (x,y)
+
+                    % test if lies in the obstacle map (if it exists)
                     if isempty(rrt.occgrid)
                         break;
                     end
@@ -175,54 +208,192 @@ classdef RRT < Navigation
                         continue;
                     end
                 end
-                theta = rand*2*pi;
+                theta = rrt.rand*2*pi;
                 xrand = [xy, theta]';
-                plot(xy(1), xy(2), 'o')
+                if opt.samples
+                    plot(xy(1), xy(2), '.')
+                end
 
                 % Step 4
                 % find the existing node closest in state space
 
-                vnear = rrt.graph.closest(xrand);
-                xnear = rrt.graph.coord(vnear);
+                vnear = rrt.graph.closest(xrand);   % nearest vertex
+                xnear = rrt.graph.coord(vnear);     % coord of nearest vertex
+
+                rrt.message('xrand (%g, %g) node %d', xy, vnear);
 
                 % Step 5
                 % figure how to drive the robot from xnear to xrand
                 
                 ntrials = 50;
                 
-                best = bestpath(xnear, xrand, ntrials);
+                best = rrt.bestpath(xnear, xrand, ntrials);
                 
                 xnew = best.path(:,best.k);
-                plot(xnew(1), xnew(2), '+');
-                drawnow
+                if opt.samples
+                    plot(xnew(1), xnew(2), 'o');
+                    plot2(best.path', 'r');
+                    drawnow
+                end
 
 %                 % ensure that the path is collision free
 %                 if ~rrt.clearpath(y(:,1:2))
 %                     disp('path collision');
 %                     continue;
 %                 end
-% 
-%                 xnew = y(end,:)';
-%                 if rrt.verbose
-%                     plot2(y)
-%                     plot2(xnew', 'go');
-%                     drawnow
-%                 end
 
                 % Step 7,8
                 % add xnew to the graph, with an edge to xnear
                 v = rrt.graph.add_node(xnew, vnear);
-                rrt.graph.userdata{v} = best;
-                j = j+1;
-
+                rrt.graph.setdata(v, best);
             end
-            close(h)
-        end % plan
 
-        function xy = randxy(rrt)
-            xy = rand(1,2) .* [rrt.xrange(2)-rrt.xrange(1) rrt.yrange(2)-rrt.yrange(1)] + [rrt.xrange(1) rrt.yrange(1)];
+            if opt.progress
+                close(h)
+            end
+            rrt.message('graph create done');
         end
 
+        function p_ = path(rrt, xstart, xgoal)
+        %PRM.path Find a path between two points
+        %
+        % X = R.path(START, GOAL) finds a path (Nx3) from state START (1x3) 
+        % to the GOAL (1x3).
+        %
+        % P.path(START, GOAL) as above but plots the path in 3D.  The nodes
+        % are shown as circles and the line segments are blue for forward motion
+        % and red for backward motion.
+        %
+        % Notes::
+        % - The path starts at the vertex closest to the START state, and ends
+        %   at the vertex closest to the GOAL state.  If the tree is sparse this
+        %   might be a poor approximation to the desired start and end.
+
+            g = rrt.graph;
+            vstart = g.closest(xstart);
+            vgoal = g.closest(xgoal);
+
+            % find path through the graph using A* search
+            path = g.Astar(vstart, vgoal);
+            cpath = g.coord(path);    % get coords from vertices
+
+            if nargout == 0
+                % plot the path
+                clf; hold on
+
+                plot2(cpath', 'o');     % plot the node coordinates
+                for i=2:length(path)
+                    p0 = g.coord(path(i-1));    % start of edge
+                    p1 = g.coord(path(i));      % end of edge
+                    b = g.data(path(i));        % get path data for segment
+                    % draw line with direction dependent color
+                    if b.vel > 0
+                        plot3([p0(1) p1(1)], [p0(2) p1(2)], [p0(3) p1(3)], 'b');
+                    else
+                        plot3([p0(1) p1(1)], [p0(2) p1(2)], [p0(3) p1(3)], 'r');
+                    end
+                end
+                xlabel('x'); ylabel('y'); zlabel('\theta');
+                grid
+            else
+                p_ = cpath';
+            end
+        end
+
+        function plot(rrt, varargin)
+        %RRT.plot Visualize navigation environment
+        %
+        % R.plot() displays the navigation tree in 3D.
+
+            clf
+            rrt.graph.plot('noedges', 'MarkerSize', 6, 'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'g');
+            hold on
+            for i=2:rrt.graph.n
+                b = rrt.graph.data(i);
+                plot2(b.path(:,1:b.k)')
+            end
+            xlabel('x'); ylabel('y'); zlabel('\theta');
+            grid; hold off
+        end
+
+        % required by abstract superclass
+        function next(rrt)
+        end
+
+        function s = char(rrt)
+        %RRT.char  Convert to string
+        %
+        % R.char() is a string representing the state of the RRT
+        % object in human-readable form.
+        %
+            % invoke the superclass char() method
+            s = char@Navigation(prm);
+
+            % add RRT specific stuff information
+            s = char(s, sprintf('  region: X %f : %f; Y %f : %f', rrt.xrange, rrt.yrange));
+            s = char(s, sprintf('  path time: %f', rrt.sim_time));
+            s = char(s, sprintf('  graph size: %d nodes', rrt.npoints));
+            s = char(s, char(rrt.graph) );
+            if ~isempty(rrt.vehicle)
+                s = char(s, char(rrt.vehicle) );
+            end
+        end
+
+
+    end % methods
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%    P R I V A T E    M E T H O D S
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    methods (Access='protected')
+
+        function best = bestpath(rrt, x0, xg, N)
+
+            % initial and final state as column vectors
+            x0 = x0(:); xg = xg(:);
+
+            best.d = Inf;
+            for i=1:N   % for multiple trials 
+            
+                %choose random direction of motion and random steer angle
+                if rand > 0.5
+                    vel = rrt.speed;
+                else
+                    vel = -rrt.speed;
+                end
+                steer = (2*rrt.rand - 1) * rrt.steermax;    % uniformly distributed
+                
+                % simulate motion of vehicle for this speed and steer angle which 
+                % results in a path
+                x = rrt.vehicle.run2(rrt.sim_time, x0, vel, steer)';
+                
+                %% find point on the path closest to xg
+                % distance of all path points from goal
+                d = colnorm( [bsxfun(@minus, x(1:2,:), xg(1:2)); angdiff(x(3,:), xg(3))] );
+                % the closest one
+                [dmin,k] = min(d);
+                
+                % is it the best so far?
+                if dmin < best.d
+                    % yes it is!  save it and the inputs that led to it
+                    best.d = dmin;
+                    best.path = x;
+                    best.steer = steer;
+                    best.vel = vel;
+                    best.k = k;
+                end
+            end 
+        end 
+
+        % generate a random coordinate within the working region
+        function xy = randxy(rrt)
+            xy = rrt.rand(1,2) .* [rrt.xrange(2)-rrt.xrange(1) rrt.yrange(2)-rrt.yrange(1)] + ...
+                [rrt.xrange(1) rrt.yrange(1)];
+        end
+
+        % test if a path is obstacle free
         function c = clearpath(rrt, xy)
             if isempty(rrt.occgrid)
                 c = true;
@@ -246,102 +417,6 @@ classdef RRT < Navigation
             end
         end
 
-        function s = char(rrt)
-            s = '';
-            s = strvcat(s, sprintf('RRT: %dx%d', size(rrt.occgrid)));
-            s = strvcat(s, sprintf('  graph size: %d', rrt.npoints));
-            s = strvcat(s, char(rrt.graph) );
-        end
 
-
-        function visualize(rrt, varargin)
-            %visualize@Navigation(rrt);
-            clf
-            rrt.graph.plot('noedges', 'MarkerSize', 6, 'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'g');
-            hold on
-            for i=2:rrt.graph.n
-                b = rrt.graph.userdata{i};
-                plot2(b.path(:,1:b.k)')
-            end
-            xlabel('x');
-            ylabel('y');
-            zlabel('\theta');
-            grid
-            hold off
-        end
-
-    end % method
-end % classdef
-
-function x = pathsim(x0, u)
-
-    if nargin < 2
-        vel = 1
-    end
-
-    tmax = 1;
-    L = 0.5;
-
-    f = @(t, x) bicycle(t, x, u, L);
-
-    [t,x] = ode45(f, [0 tmax], x0(:)');
-    if nargout < 1
-        plot2(x);
-    end
-
-end
-
-function xdot = bicycle(t, x, u, L)
-
-
-    xb = x(1); yb = x(2); thb = x(3);
-    v = u(1); gamma = u(2);
-
-    xdot = v * [ cos(thb)
-                 sin(thb)
-                 tan(gamma) / L ];
-end
-
-function best = bestpath(x0, xg, N)
-if nargin < 2
-    N = 50;
-end
-
-x0 = x0(:); xg = xg(:);
-
-steermax = 1.2; % max steer angle in rads
-speed = 1;
-
-best.d = Inf;
-for i=1:N
-    % for multiple trials choose random direction of motion and random
-    % steer angle
-    if rand > 0.5
-        vel = speed;
-    else
-        vel = -speed;
-    end
-    steer = (2*rand - 1) * steermax;
-    
-    % simulate motion for this speed and steer angle
-    x = pathsim(x0, [vel steer])';
-    
-    % find the point on path closest to xg
-    d = colnorm( [bsxfun(@minus, x(1:2,:), xg(1:2)); angdiff(x(3,:), xg(3))] );
-    
-                        %d = sum( (xv(1:2)-xrand(1:2)).^2 ) + angdiff(xv(3)-xrand(3))^2;
-
-    
-    % determine closest point to our goal
-    [dmin,k] = min(d);
-    
-    % is it best so far?
-    if dmin < best.d
-        best.d = dmin;
-        best.path = x;
-        best.steer = steer;
-        best.vel = vel;
-        best.k = k;
-    end
-end
-end
+    end % private methods
+end % class
