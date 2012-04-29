db602a65dd749a3a2f543098636ab8e55d18acd2
Peter Corke <peter.i.corke@gmail.com>
2012-04-29 06:17:37 +0000

    Removed hacks to investigate friction errors in rne.

--- mex/check.m
+++ mex/check.m
@@ -7,14 +7,8 @@ rdh = p560;
 mdl_puma560akb
 rmdh = p560m;
 
-for i=1:6
-    %rdh.links(i).Jm = 0;
-    %rdh.links(i).B = 0;
-    %rdh.links(i).Tc = 0;
-    rdh.links(i).G = abs(rdh.links(i).G);
-    rmdh.links(i).Jm = 0;
-    rmdh.links(i).B = 0;
-    rmdh.links(i).Tc = 0;
+if exist('rne') == 3
+    error('need to remove rne.mex file from @SerialLink dir');
 end
 
 check1
