bf8504e9f44a876187eccc4d004188c8a8fbc8ea
Peter Corke <peter.i.corke@gmail.com>
2014-02-03 11:51:41 +0000

    New function for SO2 and SE2 rotation matrices

---
+++ rot2.m
@@ -0,0 +1,39 @@
+%ROT2 SO2 Rotation matrix
+%
+% R = ROT2(THETA) is an SO(2) rotation matrix representing a rotation of THETA 
+% radians.
+%
+% R = ROT2(THETA, 'deg') as above but THETA is in degrees.
+%
+% See also TROT2, ROTX, ROTY, ROTZ.
+
+
+% Copyright (C) 1993-2014, by Peter I. Corke
+%
+% This file is part of The Robotics Toolbox for Matlab (RTB).
+% 
+% RTB is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% RTB is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+% 
+% You should have received a copy of the GNU Leser General Public License
+% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
+
+function R = rot2(t, deg)
+
+    if nargin > 1 && strcmp(deg, 'deg')
+        t = t *pi/180;
+    end
+    
+    ct = cos(t);
+    st = sin(t);
+    R = [
+        ct  -st
+        st   ct
+        ];
