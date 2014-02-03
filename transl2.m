551d8bdc9d716fc66308d8a48c8e59e6a010b31d
Peter Corke <peter.i.corke@gmail.com>
2014-02-03 11:51:06 +0000

    Split SE2 functionality out of transl and into transl2

--- transl2.m
+++ transl2.m
@@ -1,26 +1,26 @@
-%TRANSL Create translational transform
+%TRANSL2 Create an SE2 translational transform
 %
-% T = TRANSL(X, Y, Z) is an SE(3) homogeneous transform (4x4)representing a 
+% T = TRANSL2(X, Y) is an SE2 homogeneous transform (3x3)representing a 
 % pure translation.
 %
-% T = TRANSL(P) is a homogeneous transform representing a translation or 
-% point P=[X,Y,Z]. If P (Mx3) it represents a sequence and T (4x4xM)
+% T = TRANSL2(P) is a homogeneous transform representing a translation or 
+% point P=[X,Y]. If P (Mx2) it represents a sequence and T (3x3xM)
 % is a sequence of homogenous transforms such that T(:,:,i) corresponds to
 % the i'th row of P.
 %
-% P = TRANSL(T) is the translational part of a homogeneous transform as a 
-% 3-element column vector.  If T (4x4xM) is a homogeneous transform sequence 
-% the rows of P (Mx3) are the translational component of the corresponding 
+% P = TRANSL2(T) is the translational part of a homogeneous transform as a 
+% 2-element column vector.  If T (3x3xM) is a homogeneous transform sequence 
+% the rows of P (Mx2) are the translational component of the corresponding 
 % transform in the sequence.
 %
 % Notes::
 % - Somewhat unusually this function performs a function and its inverse.  An
 %   historical anomaly.
 %
-% See also CTRAJ.
+% See also TRANSL.
 
 
-% Copyright (C) 1993-2011, by Peter I. Corke
+% Copyright (C) 1993-2014, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for Matlab (RTB).
 % 
@@ -37,29 +37,31 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with RTB.  If not, see <http://www.gnu.org/licenses/>.
 
-function T = transl(x, y, z)
+function T = transl2(x, y)
     if nargin == 1
-        if ishomog(x)
+        if ishomog2(x)
             if ndims(x) == 3
                 % transl(T)  -> P, trajectory case
-                T = squeeze(x(1:3,4,:))';
+                T = squeeze(x(1:2,3,:))';
             else
                 % transl(T)  -> P
-                T = x(1:3,4);
+                T = x(1:2,3);
             end
-        elseif length(x) == 3
+        elseif all(size(x) == [3 3])
+            T = x(1:2,3);
+        elseif length(x) == 2
             % transl(P) -> T
             t = x(:);
-            T =    [eye(3)          t(:);
-                0   0   0   1];
+            T =    [eye(2)          t(:);
+                0   0   1];
         else
             % transl(P) -> T, trajectory case
             n = numrows(x);
-            T = repmat(eye(4,4), [1 1 n]);
-            T(1:3,4,:) = x';
+            T = repmat(eye(3,3), [1 1 n]);
+            T(1:2,3,:) = x';
         end    
-    elseif nargin == 3
-        % transl(x,y,z) -> T
-        t = [x; y; z];
-        T =    rt2tr( eye(3), t);
+    elseif nargin == 2
+        % transl(x,y) -> T
+        t = [x; y];
+        T =    rt2tr( eye(2), t);        
     end
