a7b0b7a668bd30bd1ed36adf8a78af4497abf0f5
Joern Malzahn <joern.malzahn@tu-dortmund.de>
2014-03-11 13:16:00 +0000

    + gengaussjordanc.m + header information for gendotprodc.m

--- @CodeGenerator/private/gendotprodc.m
+++ @CodeGenerator/private/gendotprodc.m
@@ -1,3 +1,35 @@
+%CODEGENERATOR.GENDOTPRODC Generates a dot product C-implementation.
+%
+% CGen.gendotprodc generates a .h and a .c file in the directory
+% specified by ccodepath.
+%
+% Notes::
+% - Is called by geninvdyn if CGen has active flag genmex or genccode.
+%
+% Authors::
+%  Joern Malzahn   (joern.malzahn@tu-dortmund.de) 
+%
+% See also CodeGenerator, CodeGenerator.gengaussjordanc.
+
+% Copyright (C) 1993-2014, by Peter I. Corke
+% Copyright (C) 2014, by Joern Malzahn
+%
+% This file is part of The Robotics Toolbox for Matlab (RTB).
+%
+% RTB is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% RTB is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Leser General Public License
+% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
+%
+% http://www.petercorke.com
 function [] =  gendotprodc(CGen)
 
 funname = 'dotprod';
---
+++ @CodeGenerator/private/gengaussjordanc.m
@@ -0,0 +1,180 @@
+%CODEGENERATOR.GENGAUSSJORDANC Generates a Gauss-Jordan C-implementation.
+%
+% CGen.gengaussjordanc generates a .h and a .c file in the directory
+% specified by ccodepath.
+%
+% Notes::
+% - Is called by genfdyn if CGen has active flag genmex or genccode.
+%
+% Authors::
+%  Joern Malzahn   (joern.malzahn@tu-dortmund.de) 
+%
+% See also CodeGenerator, CodeGenerator.gendotprodc.
+
+% Copyright (C) 1993-2014, by Peter I. Corke
+% Copyright (C) 2014, by Joern Malzahn
+%
+% This file is part of The Robotics Toolbox for Matlab (RTB).
+%
+% RTB is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% RTB is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Leser General Public License
+% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
+%
+% http://www.petercorke.com
+
+function [] =  gengaussjordanc(CGen)
+
+funname = 'gaussjordan';
+funfilename = [funname,'.c'];
+hfilename = [funname,'.h'];
+srcDir = fullfile(CGen.ccodepath,'src');
+hdrDir = fullfile(CGen.ccodepath,'include');
+
+% Check for already existing gauss-jordan C-files
+if exist(fullfile(srcDir,funfilename),'file') && exist(fullfile(srcDir,funfilename),'file')
+    return;
+end
+
+% Check for existance of C-code directories
+if ~exist(srcDir,'dir')
+    mkdir(srcDir);
+end
+if ~exist(hdrDir,'dir')
+    mkdir(hdrDir);
+end
+
+% Create the function description header
+hStruct = createHeaderStruct(funname); % create header
+if ~isempty(hStruct)
+    hFString = CGen.constructheaderstringc(hStruct);
+end
+
+%% Generate C implementation file
+fid = fopen(fullfile(srcDir,funfilename),'w+');
+
+% Function description header
+fprintf(fid,'%s\n\n',hFString);
+
+% Includes
+fprintf(fid,'%s\n\n',...
+    ['#include "', hfilename,'"']);
+
+% Function
+fprintf(fid,'%s\n',['double ',funname,'(const double *input1, const double *input2, int nEl){']);
+fprintf(fid,'\t%s\n','double res = 0;');
+fprintf(fid,'\t%s\n\n','int iEl = 0;');
+fprintf(fid,'\t%s\n','for (iEl = 0; iEl < nEl; iEl++){');
+fprintf(fid,'\t\t%s\n','res += input1[iEl] * input2[iEl];');
+fprintf(fid,'\t%s\n\n','}');
+fprintf(fid,'\t%s\n','return res;');
+fprintf(fid,'%s\n','}');
+
+% Start actual function implementation
+fprintf(fid,'%s\n',['void ',funname,'(const double* inMatrix, double* outMatrix, int dim){']);
+
+fprintf(fid,'%s\n',' '); % empty line
+
+% variable declarations
+fprintf(fid,'\t%s\n','int iRow, iCol, diagIndex;');
+fprintf(fid,'\t%s\n','double diagFactor, tmpFactor;');
+
+fprintf(fid,'%s\n',' '); % empty line
+
+% input initialization
+fprintf(fid,'\t%s\n','// Make output matrix an identity matrix.');
+fprintf(fid,'\t%s\n','// Output matrix is orignally assumed to be initialized with zeros!');
+fprintf(fid,'\t%s\n','for (iRow = 0; iRow < dim; iRow++ ){');
+fprintf(fid,'\t\t%s\n','outMatrix[dim*iRow+iRow] = 1;');
+fprintf(fid,'\t%s\n','}');
+
+fprintf(fid,'%s\n',' '); % empty line
+
+% actual elimination
+fprintf(fid,'\t%s\n','for (diagIndex = 0; diagIndex < dim; diagIndex++ )');
+fprintf(fid,'\t%s\n','{');
+fprintf(fid,'\t\t%s\n','// determine diagonal factor');
+fprintf(fid,'\t\t%s\n','diagFactor = inMatrix[dim*diagIndex+diagIndex];');
+fprintf(fid,'\t\t%s\n','mexPrintf("Pivot factor is %f\n",diagFactor);');
+
+fprintf(fid,'%s\n',' '); % empty line
+        
+fprintf(fid,'\t\t%s\n','// divide column entries by diagonal factor');
+fprintf(fid,'\t\t%s\n','for (iCol = 0; iCol < dim; iCol++){');
+fprintf(fid,'\t\t\t%s\n','inMatrix[dim*iCol+diagIndex] /= diagFactor;');
+fprintf(fid,'\t\t\t%s\n','outMatrix[dim*iCol+diagIndex] /= diagFactor;');
+fprintf(fid,'\t\t%s\n','}');
+
+fprintf(fid,'%s\n',' '); % empty line
+        
+fprintf(fid,'\t\t%s\n','// perform line-by-line elimination');
+fprintf(fid,'\t\t%s\n','for (iRow = 0; iRow < dim; iRow++){');
+fprintf(fid,'\t\t\t%s\n','if (iRow != diagIndex){');
+fprintf(fid,'\t\t\t\t%s\n','tmpFactor = inMatrix[dim*diagIndex+iRow];');
+
+fprintf(fid,'%s\n',' '); % empty line
+                
+fprintf(fid,'\t\t\t\t%s\n','for(iCol = 0; iCol < dim; iCol++){');
+fprintf(fid,'\t\t\t\t%s\n','inMatrix[dim*iCol+iRow]  -= inMatrix[dim*iCol+diagIndex]*tmpFactor;');
+fprintf(fid,'\t\t\t\t%s\n','outMatrix[dim*iCol+iRow] -= outMatrix[dim*iCol+diagIndex]*tmpFactor;');
+fprintf(fid,'\t\t\t\t%s\n','}');
+fprintf(fid,'\t\t\t%s\n','}');
+fprintf(fid,'\t\t%s\n','} // line-by-line elimination');
+
+fprintf(fid,'%s\n',' '); % empty line
+        
+fprintf(fid,'\t%s\n','}');
+    
+fprintf(fid,'%s\n','}');
+
+fclose(fid);
+
+%% Generate C header file
+fid = fopen(fullfile(hdrDir,hfilename),'w+');
+
+% Function description header
+fprintf(fid,'%s\n\n',hFString);
+
+% Include guard
+fprintf(fid,'%s\n%s\n\n',...
+    ['#ifndef ', upper([funname,'_h'])],...
+    ['#define ', upper([funname,'_h'])]);
+
+fprintf(fid,'%s\n\n',['void ',funname,'(const double *inMatrix, double *outMatrix, int dim);']);
+
+% Include guard
+fprintf(fid,'%s\n',...
+    ['#endif /*', upper([funname,'_h */'])]);
+
+fclose(fid);
+
+CGen.logmsg('\t%s\n',' done!');
+
+end
+
+ %% Definition of the function description header contents
+function hStruct = createHeaderStruct(fname)
+[~,hStruct.funName] = fileparts(fname);
+hStruct.shortDescription = ['Compute the inverse of a positive definite square matrix'];
+hStruct.calls = {[hStruct.funName,'(const double* inMatrix, double* outMatrix, int dim)']};
+hStruct.detailedDescription = {'Given a positive definite square matrix of dimension dim in inMatrix,',...
+    'the function returns its inverse outMatrix. The inverse is computed using Gauss-Jordan elimination ',...
+    'without pivoting.'};
+hStruct.inputs = { ['inMatrix: array of doubles storing [dim x dim] elements of the input square matrix column by column,'],...
+                    'dim: dimension of the square matrix.'};
+hStruct.outputs = {['outMatrix: array of doubles storing [dim x dim] elements of the output square matrix column by column.'],...
+                    'Note: the outMatrix is assumed to be initialized with zeros!'};
+hStruct.references = {'The C Book: http://publications.gbdirect.co.uk/c_book/'};
+hStruct.authors = {'This is an autogenerated function!',...
+    'Code generator written by:',...
+    'Joern Malzahn (joern.malzahn@tu-dortmund.de)'};
+hStruct.seeAlso = {'CodeGenerator'};
+end
