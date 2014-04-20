ce6d097b7c6afa2520ecd733cfed14994b591b7b
Peter Corke <peter.i.corke@gmail.com>
2014-04-20 08:08:19 +0000

    Some simple planar robot models as used in the MOOC

---
+++ mdl_planar2.m
@@ -0,0 +1,43 @@
+%MDL_PLANAR2 Create model of a simple planar 2-link mechanism
+%
+%      mdl_planar2
+%
+% Script creates the workspace variable p2 which describes the 
+% kinematic and dynamic characteristics of a simple planar 2-link mechanism.
+%
+% Also defines the vector:
+%   qz   corresponds to the zero joint angle configuration.
+%
+% Notes::
+% - No dynamics in this model
+%
+% See also SerialLink, mdl_twolink.
+
+
+% Copyright (C) 1993-2011, by Peter I. Corke
+%
+% This file is part of The Robotics Toolbox for Matlab (RTB).
+% 
+% RTB is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% RTB is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+% 
+% You should have received a copy of the GNU Leser General Public License
+% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
+
+
+a1 = 1;
+a2 = 1;
+
+p2 = SerialLink([
+    Revolute('d', 0, 'a', a1, 'alpha', 0, 'standard')
+    Revolute('d', 0, 'a', a2, 'alpha', 0, 'standard')
+    ], ...
+    'name', 'two link');
+qz = [0 0];
