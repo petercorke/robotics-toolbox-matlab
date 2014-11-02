9e66c0b9bbbefcb19137fd4af3a39d2f3f39e4de
Peter Corke <peter.i.corke@gmail.com>
2014-11-02 08:09:51 +0000

    Updated doco

--- @SerialLink/collisions.m
+++ @SerialLink/collisions.m
@@ -1,47 +1,35 @@
-%COLLISIONS Conduct collision checking for SerialLink objects
+%SerialLink.COLLISIONS Perform collision checking
 %
-% Uses the point data stored in the points property of SerialLink
-% objects to conduct point-primitive collision checking with objects of
-% the CollisionModel class. The function does not currently check the
-% base of the SerialLink object!
+% C = R.collisions(Q, MODEL) returns true if the SerialLink object R at
+% pose Q (1xN) will intersect the solid model MODEL which belongs to the
+% class CollisionModel.
 %
+% C = R.collisions(Q, MODEL, DYNMODEL, TDYN) as above but also checks
+% dynamic collision model whose elements are at pose TDYN.
+%
+% C = R.collisions(Q, MODEL, DYNMODEL) as above but assumes TDYN is the
+% robot's tool frame.
+%
+% Trajectory operation::
+%
+% If Q is MxN it is taken as a pose sequence and C is Mx1 and the collision
+% value applies to the pose of the corresponding row of Q.
+%
+% Notes::
+% - Requires the pHRIWARE package which defines CollisionModel class.
+% - The function does not currently check the base of the SerialLink
+%   object.
+% - If MODEL is [] then no static objects are assumed.
+%
+% Author::
+% Bryan Moutrie
+%
+% See also CollisionModel, SerialLink.
+
 % Copyright (C) Bryan Moutrie, 2013-2014
 % Licensed under the GNU Lesser General Public License
 % see full file for full statement
 %
-% This file requires file(s) from The Robotics Toolbox for MATLAB (RTB)
-% by Peter Corke (www.petercorke.com), see file for statement
-%
-% Syntax:
-%  (1) c = robot.collisions(q, cmdl)
-%  (2) c = robot.collisions(q, cmdl, dyn_cmdl, dyn_T)
-%  (3) c = robot.collisions(q, cmdl, dyn_cmdl)
-%  (4) c = robot.collisions(q, [], ...)
-%
-%  (2) is as per (1) but also checks CollisionModel dyn_cmdl, whose 
-%       primitives move as specified by the frames in dyn_T
-%  (3) is as per (2) but assumes dyn_cmdl is the robot's tool, i.e. the
-%       the robot's end-effector frame (including tool transform) is
-%       used in lieu of dyn_T
-%  (4) is as per (2) or (3) but with no static objects
-%
-% Outputs:
-%  c : mx1 vector, where m is the rows of q. Elements are false if no
-%       collision exists or true if a collision exists
-%
-% Inputs:
-%  q        : mxn matrix of m joint configurations of the robot, which
-%              has n links
-%  cmdl     : CollisionModel object, which is static
-%  dyn_cmdl : CollisionModel object which move according to dyn_T
-%  dyn_T    : Transforms applied to the primitives before the transform
-%              of their own property being applied. The size of dyn_T
-%              should be 4x4xmxp, where p is the number of primitives
-%              in dyn_cmdl. The ith shape in dyn_cmdl corresponds to the
-%              ith value of the 4th dimension. Use cat to create dyn_T.
-%
-% See also cat CollisionModel SerialLink
-
 % LICENSE STATEMENT:
 %
 % This file is part of pHRIWARE.
@@ -58,99 +46,97 @@
 %
 % You should have received a copy of the GNU Lesser General Public 
 % License along with pHRIWARE.  If not, see <http://www.gnu.org/licenses/>.
-%
-% RTB LIBRARY:
-%
-% Copyright (C) 1993-2014, by Peter I. Corke
-% http://www.petercorke.com
-% Released under the GNU Lesser General Public license
 
 function c = collisions(robot, q, cmdl, dyn_cmdl, dyn_T)
-
-% VERSION WITH BASE CHECKING
-% pts = robot.points;
-pts = robot.points(end-robot.n+1:end);
-for i = length(pts): -1: 1
-    numPts(i) = size(pts{i},1);
-    pts{i}(:,4) = 1;
-    pts{i} = pts{i}';
-end
-
-if isempty(cmdl), checkfuns = []; else checkfuns = cmdl.checkFuns; end
-if nargin > 3
-    dyn_checkfuns = dyn_cmdl.checkFuns;
-    if nargin == 4 || isempty(dyn_T)
-        tool = robot.tool;
-        dyn_T = [];
-    end
-else
-    dyn_checkfuns = [];
-end
-
-% VERSION WITH BASE CHECKING
-% base = length(pts) - robot.n;
-% switch base
-%     case 0
-%         % No base
-%     case 1
-%         T = robot.base;
-%         trPts = T * pts{1};
-%         points = trPts (1:3,:)';
-%         if any(checkfuns{1}(points(:,1),points(:,2),points(:,3)))
-%             C(:) = 1;
-%             display('Base is colliding');
-%             return;
-%         end
-%     otherwise
-%         error('robot has missing or extra points');
-% end
-
-poses = size(q,1);
-c = false(poses, 1);
-
-nan = any(isnan(q),2);
-c(nan) = true;
-notnan = find(~nan)';
-
-T0 = robot.base;
-
-for p = notnan
-    T = T0;
-    prevPts = 0;
-    for i = 1: robot.n;
-        T = T * robot.links(i).A(q(p,i));
-        if numPts(i) % Allows some links to be STLless
-            %       VERSION WITH BASE CHECKING
-            %       nextPts = prevPts+numPts(i+base);
-            %       trPts(:,prevPts+1:nextPts) = T * pts{i+base};
-            nextPts = prevPts+numPts(i);
-            trPts(:,prevPts+1:nextPts) = T * pts{i};
-            prevPts = nextPts;
-        end
+    
+    if ~exist('pHRIWARE')
+        error('rtb:collisions:nosupport', 'You need to install pHRIWARE in order to use this functionality');
     end
     
-    % Does same thing as cmdl.collision(trPoints(1:3,:)'), but
-    % Does not have to access object every time - quicker
-    for i = 1: length(checkfuns)
-        if any(checkfuns{i}(trPts(1,:), trPts(2,:), trPts(3,:)))
-            c(p) = true;
-            break;
-        end
+    % VERSION WITH BASE CHECKING
+    % pts = robot.points;
+    pts = robot.points(end-robot.n+1:end);
+    for i = length(pts): -1: 1
+        numPts(i) = size(pts{i},1);
+        pts{i}(:,4) = 1;
+        pts{i} = pts{i}';
     end
     
-    % Then check the dynamic collision models, if any
-    for i = 1: length(dyn_checkfuns)
-        if isempty(dyn_T)
-            dyn_trPts = T*tool \ trPts;
-        else
-            dyn_trPts = dyn_T(:,:,p,i) \ trPts;
+    if isempty(cmdl), checkfuns = []; else checkfuns = cmdl.checkFuns; end
+    if nargin > 3
+        dyn_checkfuns = dyn_cmdl.checkFuns;
+        if nargin == 4 || isempty(dyn_T)
+            tool = robot.tool;
+            dyn_T = [];
         end
-        if any(dyn_checkfuns{i}(dyn_trPts(1,:), dyn_trPts(2,:), ...
-                dyn_trPts(3,:)))
-            c(p) = true;
-            break;
-        end
+    else
+        dyn_checkfuns = [];
     end
     
-end
+    % VERSION WITH BASE CHECKING
+    % base = length(pts) - robot.n;
+    % switch base
+    %     case 0
+    %         % No base
+    %     case 1
+    %         T = robot.base;
+    %         trPts = T * pts{1};
+    %         points = trPts (1:3,:)';
+    %         if any(checkfuns{1}(points(:,1),points(:,2),points(:,3)))
+    %             C(:) = 1;
+    %             display('Base is colliding');
+    %             return;
+    %         end
+    %     otherwise
+    %         error('robot has missing or extra points');
+    % end
+    
+    poses = size(q,1);
+    c = false(poses, 1);
+    
+    nan = any(isnan(q),2);
+    c(nan) = true;
+    notnan = find(~nan)';
+    
+    T0 = robot.base;
+    
+    for p = notnan
+        T = T0;
+        prevPts = 0;
+        for i = 1: robot.n;
+            T = T * robot.links(i).A(q(p,i));
+            if numPts(i) % Allows some links to be STLless
+                %       VERSION WITH BASE CHECKING
+                %       nextPts = prevPts+numPts(i+base);
+                %       trPts(:,prevPts+1:nextPts) = T * pts{i+base};
+                nextPts = prevPts+numPts(i);
+                trPts(:,prevPts+1:nextPts) = T * pts{i};
+                prevPts = nextPts;
+            end
+        end
+        
+        % Does same thing as cmdl.collision(trPoints(1:3,:)'), but
+        % Does not have to access object every time - quicker
+        for i = 1: length(checkfuns)
+            if any(checkfuns{i}(trPts(1,:), trPts(2,:), trPts(3,:)))
+                c(p) = true;
+                break;
+            end
+        end
+        
+        % Then check the dynamic collision models, if any
+        for i = 1: length(dyn_checkfuns)
+            if isempty(dyn_T)
+                dyn_trPts = T*tool \ trPts;
+            else
+                dyn_trPts = dyn_T(:,:,p,i) \ trPts;
+            end
+            if any(dyn_checkfuns{i}(dyn_trPts(1,:), dyn_trPts(2,:), ...
+                    dyn_trPts(3,:)))
+                c(p) = true;
+                break;
+            end
+        end
+        
+    end
 end
