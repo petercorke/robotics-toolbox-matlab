a0084db28a6c214e3fe0004538f2029307bdda76
Peter Corke <peter.i.corke@gmail.com>
2015-01-24 22:28:38 +0000

    Add joystick input animation to examples.

---
+++ examples/joytest.m
@@ -0,0 +1,31 @@
+
+% Copyright (C) 1993-2014, by Peter I. Corke
+%
+% This file is part of The Robotics Toolbox for MATLAB (RTB).
+% 
+% RTB is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% RTB is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+% 
+% You should have received a copy of the GNU Leser General Public License
+% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
+%
+% http://www.petercorke.com
+
+mdl_puma560
+
+T = p560.fkine(qn);
+q = qn;
+
+while true
+    T=joy2tr(T, 'tool');
+    trprint(T, 'fmt', '%.1f')
+    q = p560.ikine6s(T, q);
+    p560.plot(q)
+end
