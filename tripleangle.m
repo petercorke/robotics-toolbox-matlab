4e00e62b4f18f022207fd595223bf3b44805acea
Peter Corke <peter.i.corke@gmail.com>
2016-03-19 09:53:37 +1000

    Triple angle visualizer (with spitfire plus gimbals) Can be built as a MATLAB app

--- tripleangle.m
+++ tripleangle.m
@@ -41,7 +41,7 @@ function varargout = tripleangle(varargin)
 
 
 
-% Last Modified by GUIDE v2.5 26-Jan-2013 15:28:14
+% Last Modified by GUIDE v2.5 19-Aug-2015 15:19:48
 
 
 % Begin initialization code - DO NOT EDIT
@@ -77,6 +77,8 @@ handles.output = hObject;
 
 figure(handles.figure1);
 
+
+% process options
 opt.wait = false;
 opt.which = {'rpy', 'euler'};
 
@@ -100,30 +102,100 @@ if length(args) > 0 && ischar(args{1})
         
     end
 end
+
+% set initial slider to axis mapping
 switch opt.which
     case 'rpy'
-        set(handles.popupmenu1, 'Value', 1);
+        set(handles.popupmenu1, 'Value', 3);
         set(handles.popupmenu2, 'Value', 2);
-        set(handles.popupmenu3, 'Value', 3);
+        set(handles.popupmenu3, 'Value', 1);
     case 'euler'
         set(handles.popupmenu1, 'Value', 3);
         set(handles.popupmenu2, 'Value', 2);
         set(handles.popupmenu3, 'Value', 3);
 end
                 
-% draw the axes
-handles.h1 = trplot(eye(3,3), 'color', 'r', args{:});
-hold on
-handles.h2 = trplot(eye(3,3), 'color', 'g', args{:});
-handles.h3 = trplot(eye(3,3), 'color', 'b', 'thick', 2, args{:});
+% draw the axes at null rotation
+%handles.h1 = trplot(eye(3,3), 'color', 'r', args{:});
 
-% Update handles structure
-guidata(hObject, handles);
 
 % set the initial value of text fields
 set(handles.text1, 'String', sprintf('%.1f', get(handles.slider1, 'Value')));
 set(handles.text2, 'String', sprintf('%.1f', get(handles.slider2, 'Value')));
 set(handles.text3, 'String', sprintf('%.1f', get(handles.slider3, 'Value')));
+
+% configure the graphics
+axis(1.5*[-1 1 -1 1 -1 1]);
+daspect([1 1 1]);
+light('Position', [0 0 5]);
+light('Position', [2 2 1]);
+grid on
+xlabel('X')
+ylabel('Y');
+zlabel('Z');
+
+% establish transforms
+handles.plane = hgtransform('Tag', 'plane');
+handles.ring1 = hgtransform('Tag', 'ring1');
+handles.ring2 = hgtransform('Tag', 'ring2');
+handles.ring3 = hgtransform('Tag', 'ring3');
+
+%% load the models
+
+% read the plane model
+fprintf('reading STL models...');
+[V,F] = stlRead( 'spitfire_assy-gear_up.stl' );
+
+% shift origin to wing centre line
+V = bsxfun(@plus, V, [0 0 75]);
+patch('Faces', F, 'Vertices', V/180, ...
+    'FaceColor', 0.8*[1.0000    0.7812    0.4975], 'EdgeAlpha', 0, ...
+    'Parent', handles.plane);
+
+%% read the gimbal rings
+
+% inner
+fprintf('.');
+[V,F] = stlRead( 'gimbal-ring1.stl' );
+patch('Faces', F, 'Vertices', V*1, ...
+    'FaceColor', 'b', 'EdgeAlpha', 0, ...
+    'Parent', handles.ring1);
+
+% middle
+fprintf('.');
+
+[V,F] = stlRead( 'gimbal-ring2.stl' );
+patch('Faces', F, 'Vertices', V*1.1, ...
+    'FaceColor', 'g',  'EdgeAlpha', 0, ...
+    'Parent', handles.ring2);
+
+% outer
+fprintf('.');
+
+[V,F] = stlRead( 'gimbal-ring3.stl' );
+patch('Faces', F, 'Vertices', V*1.1^2, ...
+    'FaceColor', 'r', 'EdgeAlpha', 0, ...
+    'Parent', handles.ring3);
+
+fprintf('\rSupermarine Spitfire Mk VIII by Ed Morley @GRABCAD\n');
+fprintf('Gimbal models by Peter Corke using OpenSCAD\n');
+
+% enable mouse-based 3D rotation
+rotate3d on
+view(50, 22);
+
+% Update handles structure
+guidata(hObject, handles);
+
+% ask for continuous callbacks
+addlistener(handles.slider1, 'ContinuousValueChange', ...
+    @(obj,event) slider1_Callback(obj, event, handles) );
+addlistener(handles.slider2, 'ContinuousValueChange', ...
+    @(obj,event) slider2_Callback(obj, event, handles) );
+addlistener(handles.slider3, 'ContinuousValueChange', ...
+    @(obj,event) slider3_Callback(obj, event, handles) );
+
+update(handles)
 
 % UIWAIT makes tripleang wait for user response (see UIRESUME)
 if opt.wait
@@ -139,7 +211,7 @@ function varargout = tripleangle_OutputFcn(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Get default command line output from handles structure
-%varargout{1} = handles.output;
+    varargout{1} = handles.output;
 
 
 % --- Executes on selection change in popupmenu1.
@@ -296,9 +368,15 @@ function update(handles)
 
 
     % display the three frames
-    trplot(handles.h1, R1);
-    trplot(handles.h2, R1*R2);
-    trplot(handles.h3, R1*R2*R3);
+    %trplot(R1*R2*R3, 'handle', handles.h1);
+    
+    set(handles.ring3, 'Matrix', r2t( R1 * roty(pi/2) ));
+    % Ry
+    set(handles.ring2, 'Matrix', r2t( R1*R2 * rotz(pi/2) ));
+    % Rx
+    set(handles.ring1, 'Matrix', r2t( R1*R2*R3 * rotx(pi/2) ));
+    
+    set(handles.plane, 'Matrix', r2t( R1*R2*R3 * roty(pi/2)*rotz(pi/2) ))
 
     
 function R = Rx(theta, which)
@@ -314,3 +392,82 @@ function R = Rx(theta, which)
         end
 
     
+
+
+% --- Executes on button press in pb_RPY.
+function pb_RPY_Callback(hObject, eventdata, handles)
+% hObject    handle to pb_RPY (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    set(handles.popupmenu1, 'Value', 3);
+    set(handles.popupmenu2, 'Value', 2);
+    set(handles.popupmenu3, 'Value', 1);
+
+% --- Executes on button press in pb_euler.
+function pb_euler_Callback(hObject, eventdata, handles)
+% hObject    handle to pb_euler (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    set(handles.popupmenu1, 'Value', 2);
+    set(handles.popupmenu2, 'Value', 3);
+    set(handles.popupmenu3, 'Value', 2);
+
+
+% --- Executes on button press in pb_top.
+function pb_top_Callback(hObject, eventdata, handles)
+% hObject    handle to pb_top (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+    view(90, 90)
+    
+% --- Executes on button press in pb_side.
+function pb_side_Callback(hObject, eventdata, handles)
+% hObject    handle to pb_side (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    view(0, 0);
+
+% --- Executes on button press in pb_front.
+function pb_front_Callback(hObject, eventdata, handles)
+% hObject    handle to pb_front (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    view(90, 0);
+
+
+% --- Executes on button press in pb_reset.
+function pb_reset_Callback(hObject, eventdata, handles)
+% hObject    handle to pb_reset (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    set(handles.slider1, 'Value', 0);
+    set(handles.text1, 'String', sprintf('%.1f', 0));
+
+    set(handles.slider2, 'Value', 0);
+    set(handles.text2, 'String', sprintf('%.1f', 0));
+
+    set(handles.slider3, 'Value', 0);
+    set(handles.text3, 'String', sprintf('%.1f', 0));
+    
+    update(handles)
+    view(50, 22);
+
+
+
+% --- Executes on button press in gimbals.
+function gimbals_Callback(hObject, eventdata, handles)
+% hObject    handle to gimbals (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of gimbals
+    if get(hObject,'Value')
+        set(handles.ring1, 'Visible', 'on');
+                set(handles.ring2, 'Visible', 'on');
+        set(handles.ring3, 'Visible', 'on');
+    else
+                set(handles.ring1, 'Visible', 'off');
+                set(handles.ring2, 'Visible', 'off');
+        set(handles.ring3, 'Visible', 'off');
+    end
