abf3a1ca086b9e1a05cdfc15547be7961beb8ba6
Peter Corke <peter.i.corke@gmail.com>
2016-03-19 09:38:34 +1000

    new unit tests

---
+++ unit_test/SE2Test.m
@@ -0,0 +1,246 @@
+function tests = SO3Test
+    tests = functiontests(localfunctions);
+end
+
+
+% we will assume that the primitives rotx,trotx, etc. all work
+
+
+function constructor_test(tc)
+    
+    verifyClass(tc, SE2(), 'SE2');
+
+    %% null
+    verifyEqual(tc, SE2().double, eye(3,3));
+    
+    %% translation only
+    t = [1 2];
+    verifyEqual(tc, SE2(t).double, transl2(t));
+    verifyEqual(tc, SE2(t').double, transl2(t));
+    verifyEqual(tc, SE2(t(1), t(2)).double, transl2(t));
+    
+    %% R
+    R = rot2(-pi/2);
+    verifyEqual(tc, SE2(R).double, r2t(R));
+    
+    %% R,t
+
+    verifyEqual(tc, SE2(R,t).double, transl2(t)*r2t(R));
+    
+    %% T
+    T = transl2(1, 2)*trot2(0.3);
+    verifyEqual(tc, SE2(T).double, T);
+    
+    %% x,y,theta
+    T = transl2(1, 2)*trot2(0.3);
+    verifyEqual(tc, SE2(1, 2, 0.3).double, T);
+    verifyEqual(tc, SE2([1, 2, 0.3]).double, T);
+    verifyEqual(tc, SE2([1, 2], 0.3).double, T);
+        
+    %% T
+    T = rt2tr(R,t);
+    verifyEqual(tc, SE2(T).double, T);
+    
+    %% copy constructor
+    TT = SE2(T);
+    verifyEqual(tc, SE2(TT).double, T);
+    
+    
+    %% vectorised versions
+    
+    T1 = transl2(1,2) * trot2(0.3);
+    T2 = transl2(1,-2) * trot2(-0.4);
+    
+    TT = cat(3, T1, T2, T1, T2);
+
+    tt = SE2(TT);
+    verifyEqual(tc, length(tt), size(TT, 3) );
+    verifyEqual(tc, tt.T, TT);
+    
+end
+
+function staticconstructors_test(tc)
+    
+    %% exponential
+    verifyEqual(tc, SO2.exp( skew(0.3) ).R, rot2(0.3), 'AbsTol', 1e-10  );
+    
+        
+    %% exponential
+    verifyEqual(tc, SE3.exp(zeros(4,4)).T, eye(4,4), 'AbsTol', 1e-10  );
+    t= [1 2 3];
+    verifyEqual(tc, SE3.exp([t 0 0 0]').T, transl(t), 'AbsTol', 1e-10  );
+end
+
+function isa_test(tc)
+    
+    verifyTrue(tc, SE2.isa(trot2(0)) );
+    verifyFalse(tc, SE2.isa(1) )
+end
+
+function resulttype_test(tc)
+    
+    t = SE2();
+    verifyClass(tc, t, 'SE2');
+    
+    verifyClass(tc, t*t, 'SE2');
+    
+    verifyClass(tc, t/t, 'SE2');
+    
+    verifyClass(tc, inv(t), 'SE2');
+    end
+
+function inverse_test(tc)    
+    
+    T1 = transl2(1, 2) * trot2(0.3);
+    TT1 = SE2(T1);
+    
+    % test inverse
+    verifyEqual(tc, double(TT1.inv()), inv(T1), 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, double(TT1*TT1.inv()), eye(3,3), 'AbsTol', 1e-10  );
+    verifyEqual(tc, double(TT1.inv()*TT1), eye(3,3), 'AbsTol', 1e-10  );
+    
+    % vector case
+    verifyEqual(tc, double(TT1.inv()*TT1), eye(3,3), 'AbsTol', 1e-10  );
+end
+
+% function uminus_test(tc)
+%     R1 = rpy2r( randn(1,3) );  t1 = randn(3,1); T1 = rt2tr(R1, t1);
+%     
+%     TT1 = SE3(T1);
+% 
+%     
+%     TM = - TT1;
+%     verifyEqual(tc, TM.double, -T1, 'AbsTol', 1e-10  );
+% end
+
+function Rt_test(tc)
+    
+    R1 = rpy2r( randn(1,3) );  t1 = randn(3,1); T1 = rt2tr(R1, t1);
+    
+    TT1 = SE3(T1);
+    
+    verifyEqual(tc, TT1.T, T1, 'AbsTol', 1e-10  );
+    verifyEqual(tc, TT1.R, R1, 'AbsTol', 1e-10  );
+    verifyEqual(tc, TT1.t, t1, 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, TT1.transl, t1', 'AbsTol', 1e-10  );
+    TT = [TT1 TT1 TT1];
+    verifyEqual(tc, TT.transl, [t1 t1 t1]', 'AbsTol', 1e-10  );
+end
+
+
+function arith_test(tc)
+    
+    R1 = rpy2r( randn(1,3) );  t1 = randn(3,1); T1 = rt2tr(R1, t1);
+    R2 = rpy2r( randn(1,3) );  t2 = randn(3,1); T2 = rt2tr(R2, t2);
+    
+    TT1 = SE3(T1);
+    TT2 = SE3(T2);
+
+    I = SE3();
+    
+    
+    %% SE3-SE3 product
+    % scalar x scalar
+    
+    verifyEqual(tc, double(TT1*TT2), T1*T2, 'AbsTol', 1e-10  );
+    verifyEqual(tc, double(TT2*TT1), T2*T1, 'AbsTol', 1e-10  );
+    verifyEqual(tc, double(TT1*I), T1, 'AbsTol', 1e-10  );
+    verifyEqual(tc, double(TT2*I), T2, 'AbsTol', 1e-10  );
+    
+    % vector x vector
+    verifyEqual(tc, [TT1 TT1 TT2] * [TT2 TT1 TT1], [TT1*TT2 TT1*TT1 TT2*TT1]);
+    
+    % scalar x vector
+    verifyEqual(tc, TT1 * [TT2 TT1], [TT1*TT2 TT1*TT1]);
+    
+    % vector x scalar
+    verifyEqual(tc, [TT1 TT2]*TT2, [TT1*TT2 TT2*TT2]);
+    
+    %% SE3-vector product
+    vx = [1 0 0]'; vy = [0 1 0]'; vz = [0 0 1]';
+
+    % scalar x scalar
+    
+    verifyEqual(tc, TT1*vy, h2e( T1*e2h(vy) ), 'AbsTol', 1e-10);
+    
+    % vector x vector
+    verifyEqual(tc, [TT1 TT2 TT1] * [vz vx vy], [h2e(T1*e2h(vz)) h2e(T2*e2h(vx)) h2e(T1*e2h(vy))], 'AbsTol', 1e-10);
+    
+    % scalar x vector
+    verifyEqual(tc, TT1 * [vx vy vz], h2e( T1*e2h([vx vy vz]) ), 'AbsTol', 1e-10);
+    
+    % vector x scalar
+    verifyEqual(tc, [TT1 TT2 TT1] * vy, [h2e(T1*e2h(vy)) h2e(T2*e2h(vy)) h2e(T1*e2h(vy))], 'AbsTol', 1e-10);
+    
+end
+
+
+function adjoint_test(tc)
+    R = rpy2r( randn(1,3) );  t = randn(3,1); T = rt2tr(R, t);
+    TT = SE3(T);
+    
+    verifyEqual(tc, TT.Ad, [R skew(t)*R; zeros(3,3) R]);
+    
+    % velxform
+    verifyEqual(tc, TT.velxform, [R zeros(3,3); zeros(3,3) R]);
+
+    
+end
+
+function interp_test(tc)
+    R = rpy2r( randn(1,3) );  t = randn(3,1); T = rt2tr(R, t);
+    TT = SE3(T);
+    I = SE3();
+    
+    verifyEqual(tc, double(interp(I, TT, 0)),   double(I), 'AbsTol', 1e-10 );
+    verifyEqual(tc, double(interp(I, TT, 1)),   double(TT), 'AbsTol', 1e-4 );
+    verifyEqual(tc, double(interp(I, TT, 0.5)), double(trinterp(T, 0.5)), 'AbsTol', 1e-10  );
+    
+end
+
+function conversions_test(tc)
+    
+    
+    
+    %%  SO2                     convert to SO2 class
+
+    T = SE2(1, 2, 0.3);
+    verifyEqual(tc, double(T.SO2), rot2(0.3), 'AbsTol', 1e-10 );
+
+end
+
+function miscellany_test(tc)
+    
+    TT = SE2(1, 2, 0.3);
+    
+    verifyEqual(tc, dim(TT), 3);
+        
+    verifyEqual( tc, isSE(TT), true );
+    
+    verifyClass(tc, TT.new, 'SE2');
+
+    verifyClass(tc, SE2.check(TT), 'SE2');
+    verifyClass(tc, SE2.check(TT.T), 'SE2');
+    z = SE2.check(TT);
+    verifyEqual(tc, double(z), double(TT));
+    
+    z = SE2.check(TT.T);
+    verifyEqual(tc, double(z), TT.T);
+    
+end
+
+
+function display_test(tc)
+    
+    r = SE2( 1, 2, 0.3 );
+    
+    r.print
+    trprint(r)   % old style syntax
+    
+    r.plot
+    
+    %r.animate  no 2D animation method
+end
+
---
+++ unit_test/SE3Test.m
@@ -0,0 +1,305 @@
+function tests = SO3Test
+    tests = functiontests(localfunctions);
+end
+
+
+% we will assume that the primitives rotx,trotx, etc. all work
+
+
+function constructor_test(tc)
+    
+    verifyClass(tc, SE3(), 'SE3');
+
+    
+    %% null
+    verifyEqual(tc, SE3().double, eye(4,4));
+    
+    %% translation only
+    t = [1 2 3];
+    verifyEqual(tc, SE3(t).double, transl(t));
+    verifyEqual(tc, SE3(t').double, transl(t));
+    verifyEqual(tc, SE3(t(1), t(2), t(3)).double, transl(t));
+    
+    %% R,t
+    R = roty(-pi/2);
+    verifyEqual(tc, SE3(R,t).double, transl(t)*r2t(R));
+    
+    %% T
+    T = rt2tr(R,t);
+    verifyEqual(tc, SE3(T).double, T);
+    
+    %% copy constructor
+    TT = SE3(T);
+    verifyEqual(tc, SE3(TT).double, T);
+    
+    
+    %% vectorised versions
+    
+    T1 = transl(1,2,3) * rpy2tr(0.2, 0.3, 0.4);
+    T2 = transl(1,-2,3) * rpy2tr(0.2, -0.3, 0.4);
+    
+    TT = cat(3, T1, T2, T1, T2);
+
+    tt = SE3(TT);
+    verifyEqual(tc, length(tt), size(TT, 3) );
+    verifyEqual(tc, tt.T, TT);
+    
+end
+
+function staticconstructors_test(tc)
+    %% rotation primitives
+    for theta = [-pi/2 0 pi/2 pi]
+        verifyEqual(tc, SE3.Rx(theta).T, trotx(theta), 'AbsTol', 1e-10  );
+    end
+    for theta = [-pi/2 0 pi/2 pi]
+        verifyEqual(tc, SE3.Ry(theta).T, troty(theta), 'AbsTol', 1e-10  );
+    end
+    for theta = [-pi/2 0 pi/2 pi]
+        verifyEqual(tc, SE3.Rz(theta).T, trotz(theta), 'AbsTol', 1e-10  );
+    end
+    
+    for theta = [-pi/2 0 pi/2 pi]*180/pi
+        verifyEqual(tc, SE3.Rx(theta, 'deg').T, trotx(theta, 'deg'), 'AbsTol', 1e-10  );
+    end
+    for theta = [-pi/2 0 pi/2 pi]
+        verifyEqual(tc, SE3.Ry(theta, 'deg').T, troty(theta, 'deg'), 'AbsTol', 1e-10  );
+    end
+    for theta = [-pi/2 0 pi/2 pi]
+        verifyEqual(tc, SE3.Rz(theta, 'deg').T, trotz(theta, 'deg'), 'AbsTol', 1e-10  );
+    end
+    
+    %% 3 angle
+    verifyEqual(tc, SE3.rpy( 0.1, 0.2, 0.3 ).T, rpy2tr( 0.1, 0.2, 0.3 ), 'AbsTol', 1e-10  );
+    verifyEqual(tc, SE3.rpy([ 0.1, 0.2, 0.3] ).T, rpy2tr( 0.1, 0.2, 0.3 ), 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, SE3.eul( 0.1, 0.2, 0.3 ).T, eul2tr( 0.1, 0.2, 0.3 ), 'AbsTol', 1e-10  );
+    verifyEqual(tc, SE3.eul([ 0.1, 0.2, 0.3] ).T, eul2tr( 0.1, 0.2, 0.3 ), 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, SE3.rpy( 10, 20, 30, 'deg' ).T, rpy2tr( 10, 20, 30, 'deg' ), 'AbsTol', 1e-10  );
+    verifyEqual(tc, SE3.rpy([ 10, 20, 30], 'deg' ).T, rpy2tr( 10, 20, 30, 'deg' ), 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, SE3.eul( 10, 20, 30, 'deg' ).T, eul2tr( 10, 20, 30, 'deg' ), 'AbsTol', 1e-10  );
+    verifyEqual(tc, SE3.eul([ 10, 20, 30], 'deg' ).T, eul2tr( 10, 20, 30, 'deg' ), 'AbsTol', 1e-10  );
+
+    %% (theta, v)
+    th = 0.2; v = unit([1 2 3]);
+    verifyEqual(tc, SE3.angvec(th, v ).T, angvec2tr(th, v), 'AbsTol', 1e-10  );
+    verifyEqual(tc, SE3.angvec(-th, v ).T, angvec2tr(-th, v), 'AbsTol', 1e-10  );
+    verifyEqual(tc, SE3.angvec(-th, -v ).T, angvec2tr(-th, -v), 'AbsTol', 1e-10  );
+    verifyEqual(tc, SE3.angvec(th, -v ).T, angvec2tr(th, -v), 'AbsTol', 1e-10  );
+    
+    %% exponential
+    verifyEqual(tc, SE3.exp(zeros(4,4)).T, eye(4,4), 'AbsTol', 1e-10  );
+    t= [1 2 3];
+    verifyEqual(tc, SE3.exp([t 0 0 0]').T, transl(t), 'AbsTol', 1e-10  );
+
+
+end
+
+function isa_test(tc)
+    
+    verifyTrue(tc, SE3.isa(trotx(0)) );
+    verifyFalse(tc, SE3.isa(1) )
+end
+
+function resulttype_test(tc)
+    
+    t = SE3();
+    verifyClass(tc, t, 'SE3');
+    
+    verifyClass(tc, t*t, 'SE3');
+    
+    verifyClass(tc, t.*t, 'SE3');
+    % other combos all fail, test this?
+
+    verifyClass(tc, t/t, 'SE3');
+    
+    verifyClass(tc, inv(t), 'SE3');
+    end
+
+function inverse_test(tc)
+    
+    R1 = rpy2r( randn(1,3) );   t1 = randn(3,1);
+    T1 = rt2tr(R1, t1); 
+    
+    TT1 = SE3(T1);
+    
+    % test inverse
+    verifyEqual(tc, double(TT1.inv()), inv(T1), 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, double(TT1*TT1.inv()), eye(4,4), 'AbsTol', 1e-10  );
+    verifyEqual(tc, double(TT1.inv()*TT1), eye(4,4), 'AbsTol', 1e-10  );
+    
+    % vector case
+    verifyEqual(tc, double(TT1.inv()*TT1), eye(4,4), 'AbsTol', 1e-10  );
+end
+
+% function uminus_test(tc)
+%     R1 = rpy2r( randn(1,3) );  t1 = randn(3,1); T1 = rt2tr(R1, t1);
+%     
+%     TT1 = SE3(T1);
+% 
+%     
+%     TM = - TT1;
+%     verifyEqual(tc, TM.double, -T1, 'AbsTol', 1e-10  );
+% end
+
+function Rt_test(tc)
+    
+    R1 = rpy2r( randn(1,3) );  t1 = randn(3,1); T1 = rt2tr(R1, t1);
+    
+    TT1 = SE3(T1);
+    
+    verifyEqual(tc, TT1.T, T1, 'AbsTol', 1e-10  );
+    verifyEqual(tc, TT1.R, R1, 'AbsTol', 1e-10  );
+    verifyEqual(tc, TT1.t, t1, 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, TT1.transl, t1', 'AbsTol', 1e-10  );
+    TT = [TT1 TT1 TT1];
+    verifyEqual(tc, TT.transl, [t1 t1 t1]', 'AbsTol', 1e-10  );
+end
+
+
+function arith_test(tc)
+    
+    R1 = rpy2r( randn(1,3) );  t1 = randn(3,1); T1 = rt2tr(R1, t1);
+    R2 = rpy2r( randn(1,3) );  t2 = randn(3,1); T2 = rt2tr(R2, t2);
+    
+    TT1 = SE3(T1);
+    TT2 = SE3(T2);
+
+    I = SE3();
+    
+    
+    %% SE3-SE3 product
+    % scalar x scalar
+    
+    verifyEqual(tc, double(TT1*TT2), T1*T2, 'AbsTol', 1e-10  );
+    verifyEqual(tc, double(TT2*TT1), T2*T1, 'AbsTol', 1e-10  );
+    verifyEqual(tc, double(TT1*I), T1, 'AbsTol', 1e-10  );
+    verifyEqual(tc, double(TT2*I), T2, 'AbsTol', 1e-10  );
+    
+    % vector x vector
+    verifyEqual(tc, [TT1 TT1 TT2] * [TT2 TT1 TT1], [TT1*TT2 TT1*TT1 TT2*TT1]);
+    
+    % scalar x vector
+    verifyEqual(tc, TT1 * [TT2 TT1], [TT1*TT2 TT1*TT1]);
+    
+    % vector x scalar
+    verifyEqual(tc, [TT1 TT2]*TT2, [TT1*TT2 TT2*TT2]);
+    
+    %% SE3-vector product
+    vx = [1 0 0]'; vy = [0 1 0]'; vz = [0 0 1]';
+
+    % scalar x scalar
+    
+    verifyEqual(tc, TT1*vy, h2e( T1*e2h(vy) ), 'AbsTol', 1e-10);
+    
+    % vector x vector
+    verifyEqual(tc, [TT1 TT2 TT1] * [vz vx vy], [h2e(T1*e2h(vz)) h2e(T2*e2h(vx)) h2e(T1*e2h(vy))], 'AbsTol', 1e-10);
+    
+    % scalar x vector
+    verifyEqual(tc, TT1 * [vx vy vz], h2e( T1*e2h([vx vy vz]) ), 'AbsTol', 1e-10);
+    
+    % vector x scalar
+    verifyEqual(tc, [TT1 TT2 TT1] * vy, [h2e(T1*e2h(vy)) h2e(T2*e2h(vy)) h2e(T1*e2h(vy))], 'AbsTol', 1e-10);
+    
+end
+
+
+function adjoint_test(tc)
+    R = rpy2r( randn(1,3) );  t = randn(3,1); T = rt2tr(R, t);
+    TT = SE3(T);
+    
+    verifyEqual(tc, TT.Ad, [R skew(t)*R; zeros(3,3) R]);
+    
+    % velxform
+    verifyEqual(tc, TT.velxform, [R zeros(3,3); zeros(3,3) R]);
+
+    
+end
+
+function interp_test(tc)
+    R = rpy2r( randn(1,3) );  t = randn(3,1); T = rt2tr(R, t);
+    TT = SE3(T);
+    I = SE3();
+    
+    verifyEqual(tc, double(interp(I, TT, 0)),   double(I), 'AbsTol', 1e-10 );
+    verifyEqual(tc, double(interp(I, TT, 1)),   double(TT), 'AbsTol', 1e-4 );
+    verifyEqual(tc, double(interp(I, TT, 0.5)), double(trinterp(T, 0.5)), 'AbsTol', 1e-10  );
+    
+end
+
+function conversions_test(tc)
+    
+    %% 3 angle
+    verifyEqual(tc, SE3.rpy( 0.1, 0.2, 0.3 ).torpy, [ 0.1, 0.2, 0.3], 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, SE3.eul( 0.1, 0.2, 0.3 ).toeul, [ 0.1, 0.2, 0.3 ], 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, SE3.rpy( 10, 20, 30, 'deg' ).R, rpy2r( 10, 20, 30, 'deg' ), 'AbsTol', 1e-10  );
+    
+    verifyEqual(tc, SE3.eul( 10, 20, 30, 'deg' ).R, eul2r( 10, 20, 30, 'deg' ), 'AbsTol', 1e-10  );
+    
+    %% (theta, v)
+    th = 0.2; v = unit([1 2 3]);
+    a = SE3.angvec(th, v ).toangvec;
+    verifyEqual(tc, a, th, 'AbsTol', 1e-10  );
+    
+    [a,b] = SE3.angvec(th, v ).toangvec;
+    verifyEqual(tc, a, th, 'AbsTol', 1e-10  );
+    verifyEqual(tc, b, v, 'AbsTol', 1e-10  );
+    
+    %% quaternion
+    
+    R = rpy2r( 0.2, 0.3, 0.4);
+    q = UnitQuaternion(R);
+    T = SO3(R).SE3;
+    verifyClass(tc, T.UnitQuaternion, 'UnitQuaternion');
+
+    verifyEqual(tc, q, T.UnitQuaternion);
+    
+    
+    %%  SE3                     convert to SE3 class
+
+    T = rpy2tr(0.2, 0.3, 0.4);
+    verifyEqual(tc, SO3(T).SE3, SE3(T), 'AbsTol', 1e-10 );
+    verifyEqual(tc, SO3(T).T, T, 'AbsTol', 1e-10 );
+
+end
+
+
+function miscellany_test(tc)
+    
+    R = rpy2r( randn(1,3) );  t = randn(3,1); T = rt2tr(R, t);
+    TT = SE3(T);
+    
+    verifyEqual(tc, dim(TT), 4);
+        
+    verifyEqual( tc, isSE(TT), true );
+    
+    verifyClass(tc, TT.new, 'SE3');
+
+    verifyClass(tc, SE3.check(TT), 'SE3');
+    verifyClass(tc, SE3.check(T), 'SE3');
+    z = SE3.check(TT);
+    verifyEqual(tc, double(z), double(TT));
+    
+    z = SE3.check(T);
+    verifyEqual(tc, double(z), T);
+    
+end
+
+function display_test(tc)
+    
+    R = rpy2r( randn(1,3) );  t = randn(3,1); T = rt2tr(R, t);
+    TT = SE3(T);
+    
+    TT.print
+    trprint(TT)   % old style syntax
+    
+    TT.plot
+    
+    TT.animate
+end
+
