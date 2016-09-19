65a22f6043944af5020809b9a41058b1fd82d3d0
Peter Corke <peter.i.corke@gmail.com>
2016-09-19 12:36:44 +1000

    handles SE3 matrices or classes, loop rather than recurse over a trajectory

--- tr2eul.m
+++ tr2eul.m
@@ -26,68 +26,65 @@
 % Copyright (C) 1993-2015, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for MATLAB (RTB).
-% 
+%
 % RTB is free software: you can redistribute it and/or modify
 % it under the terms of the GNU Lesser General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % RTB is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU Lesser General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU Leser General Public License
 % along with RTB.  If not, see <http://www.gnu.org/licenses/>.
 %
 % http://www.petercorke.com
 
-function euler = tr2eul(R, varargin)
-
+function euler = tr2eul(RR, varargin)
+    
     opt.deg = false;
     opt.flip = false;
     opt = tb_optparse(opt, varargin);
-
-	s = size(R);
-	if length(s) > 2
-        euler = zeros(s(3), 3);
-		for i=1:s(3)
-			euler(i,:) = tr2eul(R(:,:,i));
-		end
-
-        if opt.deg
-            euler = euler * 180/pi;
-        end
-		return
-	end
-
-	euler = zeros(1,3);
-
-	% Method as per Paul, p 69.
-    % euler = [phi theta psi]
-    %
-
-	if abs(R(1,3)) < eps && abs(R(2,3)) < eps
-		% singularity
-		euler(1) = 0;
-		sp = 0;
-		cp = 1;
-		euler(2) = atan2(cp*R(1,3) + sp*R(2,3), R(3,3));
-		euler(3) = atan2(-sp * R(1,1) + cp * R(2,1), -sp*R(1,2) + cp*R(2,2));
-    else
-        % non singular
+    
+    RR = SO3.check(RR);
+    euler = zeros(length(RR),3);
+    
+    for i=1:length(RR)
         
-        % Only positive phi is returned.
-        if opt.flip
-            euler(1) = atan2(-R(2,3), -R(1,3));
+        R = RR(i).R;
+        
+        % Method as per Paul, p 69.
+        % euler = [phi theta psi]
+        %
+        
+        if abs(R(1,3)) < eps && abs(R(2,3)) < eps
+            % singularity
+            eul(1) = 0;
+            sp = 0;
+            cp = 1;
+            eul(2) = atan2(cp*R(1,3) + sp*R(2,3), R(3,3));
+            eul(3) = atan2(-sp * R(1,1) + cp * R(2,1), -sp*R(1,2) + cp*R(2,2));
         else
-            euler(1) = atan2(R(2,3), R(1,3));
+            % non singular
+            
+            % Only positive phi is returned.
+            if opt.flip
+                eul(1) = atan2(-R(2,3), -R(1,3));
+            else
+                eul(1) = atan2(R(2,3), R(1,3));
+            end
+            sp = sin(eul(1));
+            cp = cos(eul(1));
+            eul(2) = atan2(cp*R(1,3) + sp*R(2,3), R(3,3));
+            eul(3) = atan2(-sp * R(1,1) + cp * R(2,1), -sp*R(1,2) + cp*R(2,2));
         end
-		sp = sin(euler(1));
-		cp = cos(euler(1));
-		euler(2) = atan2(cp*R(1,3) + sp*R(2,3), R(3,3));
-		euler(3) = atan2(-sp * R(1,1) + cp * R(2,1), -sp*R(1,2) + cp*R(2,2));
-	end
+        
+        euler(i,:) = eul;
+    end
+    
     if opt.deg
         euler = euler * 180/pi;
     end
+end
