2554289b8a89aeca6d20f87cbf9464cb2a1523e9
Peter Corke <peter.i.corke@gmail.com>
2016-12-18 12:53:11 +1000

    change delta to be in the T0 frame doco update

--- tr2delta.m
+++ tr2delta.m
@@ -1,23 +1,24 @@
 %TR2DELTA Convert homogeneous transform to differential motion
 %
 % D = TR2DELTA(T0, T1) is the differential motion (6x1) corresponding to 
-% infinitessimal motion from pose T0 to T1 which are homogeneous 
-% transformations (4x4). D=(dx, dy, dz, dRx, dRy, dRz) and is an approximation
-% to the average spatial velocity multiplied by time.
+% infinitessimal motion (in the T0 frame) from pose T0 to T1 which are homogeneous 
+% transformations (4x4) or SE3 objects. D=(dx, dy, dz, dRx, dRy, dRz). 
 %
-% D = TR2DELTA(T) is the differential motion corresponding to the
-% infinitessimal relative pose T expressed as a homogeneous 
-% transformation.
+% D = TR2DELTA(T) as above but the motion is with respect to the world frame.
 %
 % Notes::
 % - D is only an approximation to the motion T, and assumes
 %   that T0 ~ T1 or T ~ eye(4,4).
+% - can be considered as an approximation to the effect of spatial velocity over a
+%   a time interval, average spatial velocity multiplied by time.
+%
+% Reference::
+% - Robotics, Vision & Control 2nd Edition, p67
 %
 % See also DELTA2TR, SKEW.
 
 
-
-% Copyright (C) 1993-2015, by Peter I. Corke
+% Copyright (C) 1993-2016, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for MATLAB (RTB).
 % 
@@ -38,6 +39,7 @@
 
 function delta = tr2delta(A, B)
 
+    % handle arguments, convert all to 4x4 matrices
     if nargin > 0
         if isa(A, 'SE3')
             T1 = A.double;
@@ -58,12 +60,20 @@ function delta = tr2delta(A, B)
                 error('RTB:tr2delta:badarg', 'T0 should be a homogeneous transformation');
         end
     end
-    R0 = t2r(T0); R1 = t2r(T1);
-    % in world frame
-    %[th,vec] = tr2angvec(R1*R0');
-    dR = vex(R1*R0');
-    %delta = [ (T1(1:3,4)-T0(1:3,4)); th*vec' ];
-    delta = [ (T1(1:3,4)-T0(1:3,4)); dR];
+    
+    % compute incremental transformation from T0 to T1 in the T0 frame
+    TD = inv(T0) * T1;
+
+    % build the delta vector
+    delta = [transl(TD); vex(t2r(TD) - eye(3,3))];
+    
+    
+%    R0 = t2r(T0); R1 = t2r(T1);
+%    % in world frame
+%    %[th,vec] = tr2angvec(R1*R0');
+%    dR = vex(R1*R0');
+%    %delta = [ (T1(1:3,4)-T0(1:3,4)); th*vec' ];
+%    delta = [ (T1(1:3,4)-T0(1:3,4)); dR];
 
 % same as above but more complex
 %    delta = [	T1(1:3,4)-T0(1:3,4);
