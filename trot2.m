f0044d51165f2fa0d4bb80004dc5fe5f1e1df3fb
Peter Corke <peter.i.corke@gmail.com>
2016-12-18 15:06:08 +1000

    doco update

--- trot2.m
+++ trot2.m
@@ -8,11 +8,9 @@
 % Notes::
 % - Translational component is zero.
 %
-% See also ROT2, TRANSL2, ISHOMOG2, TRPLOT2, TROTX, TROTY, TROTZ.
+% See also rot2, transl2, ishomog2, trplot2, trotx, troty, trotz, SE2.
 
-
-
-% Copyright (C) 1993-2015, by Peter I. Corke
+% Copyright (C) 1993-2016, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for MATLAB (RTB).
 % 
