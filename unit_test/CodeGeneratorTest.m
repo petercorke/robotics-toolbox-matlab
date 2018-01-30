7dab2b82f47a637b034cf21577aae1ed5591250b
Jörn Malzahn <10830038+joernmalzahn@users.noreply.github.com>
2018-01-30 15:51:44 +0100

    reduced number of trials

--- unit_test/CodeGeneratorTest.m
+++ unit_test/CodeGeneratorTest.m
@@ -40,7 +40,7 @@ function setupOnce(testCase)
     testRob = SerialLink(L, 'name', 'UnitTestRobot');
     testCase.TestData.rob = testRob.nofriction('all');
 
-    testCase.TestData.nTrials = 1000; % number of tests to perform in each subroutine
+    testCase.TestData.nTrials = 10; % number of tests to perform in each subroutine
     
     testCase.TestData.cGen = CodeGenerator(testCase.TestData.rob,'default','logfile','cGenUnitTestLog.txt');
     testCase.TestData.cGen.verbose = 0;
