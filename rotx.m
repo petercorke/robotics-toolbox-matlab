4179d9d9314108378b14613b4e657e6ba9ef5c1c
Peter Corke <peter.i.corke@gmail.com>
2018-07-08 14:23:38 -0400

    If elements are less than eps make them proper zero.  Small values can cause trouble after conversion to symbolic form. Make all functions closed with end.

--- rotx.m
+++ rotx.m
@@ -34,11 +34,24 @@ function R = rotx(t, deg)
     if nargin > 1 && strcmp(deg, 'deg')
         t = t *pi/180;
     end
-    
+        
     ct = cos(t);
     st = sin(t);
+    
+    % make almost zero elements exactly zero
+    if ~isa(t, 'sym')
+        if abs(st) < eps
+            st = 0;
+        end
+        if abs(ct) < eps
+            ct = 0;
+        end
+    end
+    
+    % create the rotation matrix
     R = [
         1   0    0
         0   ct  -st
         0   st   ct
         ];
+end
--- roty.m
+++ roty.m
@@ -33,10 +33,24 @@ function R = roty(t, deg)
     if nargin > 1 && strcmp(deg, 'deg')
         t = t *pi/180;
     end
+    
     ct = cos(t);
     st = sin(t);
+    
+    % make almost zero elements exactly zero
+    if ~isa(t, 'sym')
+        if abs(st) < eps
+            st = 0;
+        end
+        if abs(ct) < eps
+            ct = 0;
+        end
+    end
+    
+    % create the rotation matrix
     R = [
         ct  0   st
         0   1   0
        -st  0   ct
        ];
+end
