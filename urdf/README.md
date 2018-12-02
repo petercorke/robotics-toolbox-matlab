0a1574119a58c84ee950252a31e0900e8b64f8b5
Peter Corke <peter.i.corke@gmail.com>
2018-12-02 15:49:05 +0800

    Add support for branched mechanisms Greatly improve the summary info displayed Improved the doco Added an extra example file + README

---
+++ urdf/README.md
@@ -0,0 +1,4 @@
+These examples mostly from the [ROS URDF tutorial](http://wiki.ros.org/urdf/Tutorials)
+
+planar2b.udrf is a simple 2 link arm moving in the plane, similar to mdl_planar2
+branched.urdf is a variant that has two second links, ie. is branched
--- urdfparse.m
+++ urdfparse.m
@@ -28,6 +28,17 @@ function urdf = urdfparse(filename)
     
 
     %% process all the link elements
+    
+    % create a vector of joint structures, fields are:
+    % name       name of the joint
+    % type       type of joint (fixed, continuous, prismatic)
+    % T          SE3 object for pose of joint wrt link frame
+    % xyz        position of joint wrt link frame
+    % rpy        RPY angles of joint frame wrt link frame
+    % node       reference to XML DOM node for this <joint>
+    % child      link index for downstream link
+    % parent     link index for downstream link
+    
     linkNodes = robot.getElementsByTagName('link');
     for i=1:linkNodes.getLength
         node = linkNodes.item(i-1);
@@ -49,12 +60,23 @@ function urdf = urdfparse(filename)
         end
         links(i).T = SE3(t) * SE3.rpy(rpy);
         links(i).node = node;
+        links(i).children = [];
+        links(i).parent = [];
     end
     
     linkNames = [links.name];  % a list of link names
-    urdf.links = links;
+    
     
     %% process all the joint elements
+
+    % create a vector of link structures, fields are are:
+    % name       name of the link
+    % geometry   reference to XML DOM node
+    % T          SE3 object for origin of visual wrt link frame
+    % node       reference to XML DOM node for this <link>
+    % children   list of joint indexes attaching this link to its children
+    % parent     index of joint attaching this link to its parent
+    
     jointNodes = robot.getElementsByTagName('joint');
     joints = [];
     for i=1:jointNodes.getLength
@@ -64,8 +86,12 @@ function urdf = urdfparse(filename)
         try
             joints(i).axis = str2num(node.getElementsByTagName('axis').item(0).getAttribute('xyz'));
         end
-        joints(i).parent = find(strcmp(node.getElementsByTagName('parent').item(0).getAttribute('link'), linkNames));
-        joints(i).child = find(strcmp(node.getElementsByTagName('child').item(0).getAttribute('link'), linkNames));
+        p = find(strcmp(node.getElementsByTagName('parent').item(0).getAttribute('link'), linkNames));
+        c = find(strcmp(node.getElementsByTagName('child').item(0).getAttribute('link'), linkNames));
+        joints(i).parent = p;
+        joints(i).child = c;
+        links(p).children = [links(p).children i];
+        links(c).parent = i;
         
         t = [0 0 0]; rpy = [0 0 0];  % in case not provided
         try
@@ -84,26 +110,59 @@ function urdf = urdfparse(filename)
         joints(i).node = node;
     end
     urdf.joints = joints;
+    urdf.links = links;
+    
+    urdf.endlinks = find( cellfun('isempty', {links.children}) );
+    urdf.ets = [];
     
     %% create a transform list
-    transforms = "";
-
-    for joint = joints
-        transforms = transforms + xform2s(joint) + axis2s(joint);
-    end
     
-    transforms = strtrim(transforms);
-    urdf.ets = char(transforms);
+    % the mechanism might have multiple endpoints (if branched), for each
+    % endpoint compute an ETS chain and return a string array
+    
+    for endlink = urdf.endlinks        
+        transforms = "";
+        joint = joints(links(endlink).parent);
+        while true
+            transforms = xform2s(joint) + axis2s(joint) + transforms;
+            joint = joints(links(joint.parent).parent);
+            if isempty(joint)
+                break;
+            end
+        end
+        transforms = strtrim(transforms);
+        urdf.ets = [urdf.ets transforms];
+    end
     
     %% display a summary
     fprintf('\nLinks::\n');    
     for i=1:length(links)
-        fprintf('%2d: %10s; %s\n', i, links(i).name, links(i).geometry);
+        name = links(i).name;
+        if ismember(i, urdf.endlinks)
+            name = "*" + name;
+        end
+        geomlist = "";
+        if ~isempty(links(i).geometry)
+            g = links(i).geometry.getChildNodes;
+            for k=0:g.getLength-1
+                c = g.item(k);
+                if c.getNodeType == 1  % skip text nodes
+                    if geomlist == ""
+                        geomlist = "shape=";
+                    else
+                        geomlist = geomlist + ", ";
+                    end
+                    geomlist = geomlist + string(c.getNodeName);
+                end
+            end
+        end
+        fprintf('%2d: %10s; parentjoint=%s, childjoints=%s; %s\n', i, name, ...
+            formatj(links(i).parent), formatj(links(i).children), geomlist);
     end
     if ~isempty(joints)
         fprintf('\nJoints::\n');
         for i=1:length(joints)
-            fprintf('%2d: %10s; type=%s, parent=%d, child=%d\n', i, joints(i).name, joints(i).type, joints(i).parent, joints(i).child);
+            fprintf('%2d: %10s; type=%s, parentlink=%d, childlink=%d\n', i, joints(i).name, joints(i).type, joints(i).parent, joints(i).child);
         end
     end
     if ~isempty(materials)
@@ -116,6 +175,14 @@ function urdf = urdfparse(filename)
         fprintf('\nForward kinematics::\n  %s\n', transforms);
     end
 
+end
+
+function s = formatj(j)
+    if isempty(j)
+        s = '-';
+    else
+        s = strip(num2str(j, '%d,'), 'right', ',');
+    end
 end
 
 function t = xform2s(joint)
