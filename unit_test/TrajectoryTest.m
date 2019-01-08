8e7082fa9ab2d60e9b391522ef6f5dfbcc220aad
Peter Corke <peter.i.corke@gmail.com>
2019-01-08 16:49:40 +1000

    updated as part of split

--- unit_test/TrajectoryTest.m
+++ unit_test/TrajectoryTest.m
@@ -8,17 +8,6 @@ function teardownOnce(tc)
     close all
 end
 
-function trinterp2_test(tc)
-    T0 = transl2(1, 2) * trot2(20, 'deg');
-    T1 = transl2(3, 6) * trot2(60, 'deg');
-    Tm = transl2(2, 4) * trot2(40, 'deg');
-    
-    T = trinterp2(T0, T1, [0.5 0 1]);
-    tc.verifyEqual(size(T), [3 3 3]);
-    tc.verifyEqual(T(:,:,1), Tm, 'abstol', 1e-10);
-    tc.verifyEqual(T(:,:,2), T0, 'abstol', 1e-10);
-    tc.verifyEqual(T(:,:,3), T1, 'abstol', 1e-10);
-end
 
 function tpoly_test(tc)
 
