3bd7fbb6433c2b2bc87a23e61314d5068803723f
Peter Corke <peter.i.corke@gmail.com>
2019-02-24 13:38:23 +1000

    Add comments about the expected file system Build the Java and MEX files Setup the path

--- unit_test/RunAllTests.m
+++ unit_test/RunAllTests.m
@@ -1,3 +1,11 @@
+% run the tests for Travis CI
+%
+% File system looks like this:
+%    robotics-toolbox-matlab
+%    robotics-toolbox-matlab/unit_test  ** WORKING folder
+%	 lib/toolbox-common
+%    lib/spatial-math-toolbox
+
 import matlab.unittest.plugins.CodeCoveragePlugin
 import matlab.unittest.plugins.codecoverage.CoberturaFormat
 import matlab.unittest.TestRunner
@@ -10,6 +18,23 @@ reportFile = fullfile('..', 'coverage.xml');
 reportFormat = CoberturaFormat(reportFile);
 plugin = CodeCoveragePlugin.forFolder('..', 'Producing',reportFormat);
 runner.addPlugin(plugin);
+
+% setup the path
+
+% for other toolboxes
+addpath ../../lib/toolbox-common-matlab
+addpath ../../lib/spatial-math
+
+% for RTB
+addpath ..
+addpath ../models
+addpath ../data
+addpath ../simulink
+
+% build the MEX file
+cd ../mex
+make
+cd ..
 
 % Run all unit tests in my repository.
 results = runner.run(suite);
