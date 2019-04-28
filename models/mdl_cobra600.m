0f35e0b3bcd692c289c22601e6f7d196bbc509ff
Peter Corke <peter.i.corke@gmail.com>
2019-04-28 17:35:33 +1000

    Fixed H1 comment block

--- models/mdl_cobra600.m
+++ models/mdl_cobra600.m
@@ -1,24 +1,14 @@
-%MDL_PUMA560 Create model of Puma 560 manipulator
+%MDL_COBRA600 Create model of Adept Cobra 600 manipulator
 %
-% MDL_PUMA560 is a script that creates the workspace variable p560 which
-% describes the kinematic and dynamic characteristics of a Unimation Puma
-% 560 manipulator using standard DH conventions. 
+% MDL_COBRA600 is a script that creates the workspace variable c600 which
+% describes the kinematic characteristics of the 4-axis Adept Cobra 600
+% SCARA manipulator using standard DH conventions. 
 %
 % Also define the workspace vectors:
 %   qz         zero joint angle configuration
-%   qr         vertical 'READY' configuration
-%   qstretch   arm is stretched out in the X direction
-%   qn         arm is at a nominal non-singular configuration
 %
 % Notes::
 % - SI units are used.
-% - The model includes armature inertia and gear ratios.
-%
-% Reference::
-% - "A search for consensus among model parameters reported for the PUMA 560 robot",
-%   P. Corke and B. Armstrong-Helouvry, 
-%   Proc. IEEE Int. Conf. Robotics and Automation, (San Diego), 
-%   pp. 1608-1613, May 1994.
 %
 % See also SerialRevolute, mdl_puma560akb, mdl_stanford.
 
