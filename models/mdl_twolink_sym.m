f808b503d91391f0c902c6af4078321b15be36fd
Peter Corke <peter.i.corke@gmail.com>
2020-01-13 07:17:13 +1000

    suppress display of variables fix base transform, evaluate numerically to 0s and 1s, rather than later symbolically

--- models/mdl_twolink_sym.m
+++ models/mdl_twolink_sym.m
@@ -52,10 +52,10 @@ syms a1 a2 g real
 syms c1 c2 m1 m2 real
 
 %syms Iyy1 Iyy2 b1 b2 real
-Iyy1 = 0
-Iyy2 = 0
-b1 = 0
-b2 = 0
+Iyy1 = 0;
+Iyy2 = 0;
+b1 = 0;
+b2 = 0;
 
 twolink = SerialLink([
     Revolute('d', 0, 'a', a1, 'alpha', 0, 'm', m1, 'r', [c1 0 0], 'I', [0 Iyy1 0], 'B', b1, 'G', 1, 'Jm', 0, 'standard')
@@ -65,6 +65,6 @@ twolink = SerialLink([
     'comment', 'from Spong, Hutchinson, Vidyasagar');
 twolink = twolink.sym();
 twolink.gravity = [0; 0; g];
-twolink.base = trotx(sym('pi')/2);
+twolink.base = trotx(pi/2);
 
 syms q1 q2 q1d q2d q1dd q2dd real
